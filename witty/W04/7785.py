02e55853d25b8ad28ae7e69c3de3486aa5d478b7
lwittyl <mercury0502@dgu.ac.kr>
2024-08-01 23:22:17 +0900

    7785/silver5/https://www.acmicpc.net/problem/7785

---
+++ witty/W04/7785.py
@@ -0,0 +1,9 @@
+n=int(input())
+dict={}
+for i in range(n):
+    man, state=input().split()
+    if state=='enter':
+        dict[man]=True
+    else:
+        del dict[man]
+print("\n".join(sorted(dict.keys(), reverse=True)))
